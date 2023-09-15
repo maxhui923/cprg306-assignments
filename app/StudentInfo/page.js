b54d63817299eec4326e4a89c4bfdd13c5fdd7e8
maxhui923 <maxhui923@gmail.com>
2023-09-15 13:03:38 -0600

    week 2 assignment
    week 2 assignment

---
+++ app/StudentInfo/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <div>
+            <p>Name: Max Hui</p>
+            <p>Course: CPRG 306 C</p>
+            <Link href="https://github.com/maxhui923">Github Link</Link>
+        </div>
+    )
+} 
+
---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudentInfo from '../StudentInfo/page';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>My Shopping List</h1>
+      <StudentInfo/>
+
+    </main>
+  );
+}
